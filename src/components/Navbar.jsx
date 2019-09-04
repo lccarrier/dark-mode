c42d9fb32ce94585967d8794d78cfda785794baf
LC Carrier <lc@LCs-MacBook-Pro.local>
2019-09-04 19:14:57 -0400

    updated files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
+import useDarkMode from "../hooks/useDarkMode";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
   return (
     <nav className="navbar">
-      <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
+      <h1>Yolo Tracker</h1>
+      <div className="dark-mode__toggle" onClick={toggleMode}>
         <div
-          onClick={toggleMode}
           className={darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
